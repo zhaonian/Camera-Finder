257d0aa874a5cd4bce9cad102216a78abdd3efe0
Zhaonian Luan <zhaonial@uci.edu>
2017-10-23 00:50:40 -0700

    finished with error

--- Camera Localization/main.py
+++ Camera Localization/main.py
@@ -1,56 +1,39 @@
-import numpy as np
+import glob
 import cv2
-from matplotlib import pyplot as plt
+import CorrespondenceMatcher as cm
+import ProjectionEstimator as pe
+import Renderer as rr
 
-MIN_MATCH_COUNT = 10
 
-img1 = cv2.imread('IMG_6720.JPG', 0)          # queryImage
-img2 = cv2.imread('pattern.png', 0)  # trainImage
+FOCAL_LENGTH = 32
+PATTERN_SIZE = 88
 
-# Initiate SIFT detector
-sift = cv2.xfeatures2d.SIFT_create()
 
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1, None)
-kp2, des2 = sift.detectAndCompute(img2, None)
+pattern = cv2.imread('pattern.png', 0)     # trainImage
+matcher = cm.CorrespondenceMatcher(pattern)
+estimator = pe.ProjectionEstimator()
+renderer = rr.Renderer()
 
-FLANN_INDEX_KDTREE = 0
-index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-search_params = dict(checks=50)
+target_files = glob.glob("*.JPG")
 
-flann = cv2.FlannBasedMatcher(index_params, search_params)
+for img_file in target_files:
+        print("loading image " + img_file)
+        query = cv2.imread(img_file, 0)    # queryImage
 
-matches = flann.knnMatch(des1, des2, k=2)
+        q_h, q_w = query.shape[:2]
+        p_h, p_w = pattern.shape[:2]
 
-# store all the good matches as per Lowe's ratio test.
-good = []
-for m, n in matches:
-    if m.distance < 0.7 * n.distance:
-        good.append(m)
+        print("finding homography for " + img_file)
+        h = matcher.find_homography(query)
+        print("homography matrix " + str(h))
 
-if len(good) > MIN_MATCH_COUNT:
-    src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+        print("finding extrinsic camera parameter for " + img_file)
+        rx, ry, rz, px, py, pz, residual = estimator.compute_extrinsic(
+            h, q_w / p_w, FOCAL_LENGTH, PATTERN_SIZE)
+        print("rotation: " + str([rx, ry, rz]))
+        print("position: " + str([px, py, pz]))
+        print("parameter residual " + str(residual))
 
-    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-    matchesMask = mask.ravel().tolist()
-
-    h, w = img1.shape
-    pts = np.float32(
-        [[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
-    dst = cv2.perspectiveTransform(pts, M)
-
-    img2 = cv2.polylines(img2, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
-else:
-    print("Not enough matches are found - %d/%d" %
-          (len(good), MIN_MATCH_COUNT))
-    matchesMask = None
-
-draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
-                   singlePointColor=None,
-                   matchesMask=matchesMask,  # draw only inliers
-                   flags=2)
-
-img3 = cv2.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
-
-plt.imshow(img3, 'gray'), plt.show()
+        print("rendering result")
+        # renderer.render(img_file, rx, ry, rz, px, py, pz)
+        break
